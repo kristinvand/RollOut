d202a4314ed213a3aa9634e06d5bc1bcdf3c1c30
Chad McDaniel <mcdanich@mail.uc.edu>
2019-04-04 18:16:52 -0400

    Login and Register activity don't do anything yet. Pressing login takes you to maps screen

--- app/src/main/java/com/example/kristin/rollout/RegisterActivity.java
+++ app/src/main/java/com/example/kristin/rollout/RegisterActivity.java
@@ -1,7 +1,13 @@
 package com.example.kristin.rollout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText etPhoneNumber = (EditText) findViewById(R.id.etPhoneNumber);
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+        final TextView loginLink = (TextView) findViewById(R.id.tvLogin);
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                RegisterActivity.this.startActivity(loginIntent);
+
+
+            }
+        });
+
+
     }
 }
